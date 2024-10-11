75d444cec7d65134c79c2cb0ca603eb51882a386
Kokiben <bennassarkaoutar@gmail.com>
2024-10-11 01:16:04 +0000

    definevar

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Function returns the string representation of a float."""
+
+
+def to_str(n: float) -> str:
+    """Returns the string representation of a float."""
+    return str(n)
