4ed56f20220816c0cc0527536b3bbf0e72ed47c2
Kokiben <bennassarkaoutar@gmail.com>
2024-10-14 22:41:29 +0000

    py

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python3
-"""Contains a function that spawns Tasks multiple times with a
-specified maximum delay between each call."""
+"""Contains a function that spawns multiple Tasks with a
+specified maximum delay between each invocation."""
 import asyncio
 from typing import List
 
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-def task_wait_n(n: int, max_delay: int) -> List[float]:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """Creates n tasks using task_wait_random with a specified maximum delay.
     
     Args:
-        n: The total number of tasks to create.
+        n: The number of tasks to create.
         max_delay: The maximum delay for each task.
         
     Returns:
-        A list containing the delays in ascending order.
+        A list of delays sorted in ascending order.
     """
-    wait_tasks = [task_wait_random(max_delay) for _ in range(n)]  # Generate the tasks
-    return [asyncio.run(wait_task) for wait_task in asyncio.as_completed(wait_tasks)]  # Await and return the completed tasks
+    wait_tasks = [task_wait_random(max_delay) for _ in range(n)]  # Create the tasks
+    return [await wait_task for wait_task in asyncio.as_completed(wait_tasks)]  # Await the completion of tasks and return the results
