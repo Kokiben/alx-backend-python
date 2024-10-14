156317425fed4d494531749c0095f16e92f2ffe3
Kokiben <bennassarkaoutar@gmail.com>
2024-10-14 22:00:48 +0000

    async

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -4,6 +4,7 @@
 import asyncio
 import random
 
+
 async def wait_random(max_delay: int = 10) -> float:
     """
     Generates a random wait time between 0 and max_delay seconds and pauses for that duration.
